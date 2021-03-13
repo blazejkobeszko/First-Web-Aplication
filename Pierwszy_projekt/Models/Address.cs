374f10bdf6dd099010874bc2af98af3065a9be19
Błażej <blazejkob1342@gmail.com>
2021-03-13 19:21:42 +0100

    Dodaj pliki projektów.

---
+++ Pierwszy_projekt/Models/Address.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pierwszy_projekt.Models
+{
+    public class Address
+    {
+       // [Display(Name = "Ulica")]
+        [StringLength(60, MinimumLength = 3)]
+       // [Required(ErrorMessage ="Pole jest obowiązkowe")]
+
+        public string Street { get; set; }
+
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public int Number { get; set; }
+
+    }
+}
---
+++ Pierwszy_projekt/Pages/Index.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Pierwszy_projekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pierwszy_projekt.Pages
+{
+
+    public class IndexModel : PageModel
+    {
+
+
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public Address Address { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+
+                Name = "User";
+
+
+            }
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+
+    }
+}
