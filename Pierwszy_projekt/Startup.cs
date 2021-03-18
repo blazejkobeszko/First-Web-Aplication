062cc34083de9e8121e3dd85794345296e29e106
Błażej <blazejkob1342@gmail.com>
2021-03-18 23:27:00 +0100

    Revert "Działąjaca wersja z obsługą sesji"
    This reverts commit cc9684a31a7ed0ef09f7196783d4f1c3e5dbc337.

--- Pierwszy_projekt/Startup.cs
+++ Pierwszy_projekt/Startup.cs
@@ -24,14 +24,6 @@ namespace Pierwszy_projekt
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddMemoryCache();
-            services.AddSession(options =>
-            {
-                options.IdleTimeout = TimeSpan.FromSeconds(10);
-                options.Cookie.HttpOnly = true;
-                options.Cookie.IsEssential = true;
-            });
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,9 +44,9 @@ namespace Pierwszy_projekt
             app.UseStaticFiles();
 
             app.UseRouting();
-            
+
             app.UseAuthorization();
-            app.UseSession();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
